32e17fe127fde935d43b4c4dba9ed446725325ec
twfabdel <twfabdel@gmail.com>
2016-11-19 00:40:01 -0500

    preliminary list filtering for grocery

--- AfrIdge/GroceryListViewController.swift
+++ AfrIdge/GroceryListViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
-class GroceryListViewController: UIViewController {
+class GroceryListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+
+    @IBOutlet weak var list: UITableView!
+    @IBOutlet weak var ticker: UISegmentedControl!
+    var unchecked: [String] = ["milk", "eggs", "yogurt", "cheese"]
+    var checked: [String] = ["chicken", "lettuce", "tomatoes"]
+    var index = 0
+    
+    //Change which list is being displayed
+    @IBAction func changeListFilter(_ sender: UISegmentedControl) {
+        index = ticker.selectedSegmentIndex
+        print(index)
+        list.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(index == 0) {
+            return(checked.count)
+        }
+        return(unchecked.count)
+    }
+ 
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        
+        if(index == 0) {
+            cell.textLabel?.text = checked[indexPath.row]
+        } else {
+            cell.textLabel?.text = unchecked[indexPath.row]
+        }
+        return(cell)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +59,7 @@ class GroceryListViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         print("In Grocery List!")
     }
-
+    
+    
 }
 
