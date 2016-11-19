62e437ca22556b67e36c52b5e7c9951e93a2576d
twfabdel <twfabdel@gmail.com>
2016-11-18 22:17:05 -0500

    First commit

---
+++ AfrIdge/InventoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InventoryViewController.swift
+//  AfrIdge
+//
+//  Created by Tarek Abdelghany on 11/18/16.
+//  Copyright © 2016 AfrIdge. All rights reserved.
+//
+
+import UIKit
+
+class InventoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("In Inventory!")
+    }
+
+
+}
+
---
+++ AfrIdge/RecipesViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RecipesViewController.swift
+//  AfrIdge
+//
+//  Created by Tarek Abdelghany on 11/18/16.
+//  Copyright © 2016 AfrIdge. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("In Recipes!")
+    }
+    
+}
