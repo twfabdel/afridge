c39925fd6ddbcdcbda154589a2cbdca8044acbcd
twfabdel <twfabdel@gmail.com>
2016-12-02 13:17:07 -0800

    added cancel btn

--- AfrIdge/DoneShoppingViewController.swift
+++ AfrIdge/DoneShoppingViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class DoneShoppingViewController: UIViewController {
     
     @IBOutlet weak var popUp: UIView!
-
+    @IBOutlet weak var cancelBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,15 +26,9 @@ class DoneShoppingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancel(_ sender: Any) {
+        self.view.removeFromSuperview()
     }
-    */
+    
 
 }
